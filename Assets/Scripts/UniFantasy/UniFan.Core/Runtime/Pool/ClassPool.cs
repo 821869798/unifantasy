77c8e9033980eed3702615099db062c8c1cb624d
bufan <821869798@qq.com>
2023-11-07 16:46:56 +0800

    优化ClassPool

--- Assets/Scripts/UniFantasy/UniFan.Core/Runtime/Pool/ClassPool.cs
+++ Assets/Scripts/UniFantasy/UniFan.Core/Runtime/Pool/ClassPool.cs
@@ -8,6 +8,10 @@ namespace UniFan
     {
         private static Dictionary<Type, Stack<IReusableClass>> _reusablePool = new Dictionary<Type, Stack<IReusableClass>>();
 
+#if UNITY_EDITOR
+        private static Dictionary<Type, HashSet<IReusableClass>> _reusableCheckPool = new Dictionary<Type, HashSet<IReusableClass>>();
+#endif
+
         public static T Get<T>() where T : class, IReusableClass, new()
         {
             Type type = typeof(T);
@@ -16,13 +20,25 @@ namespace UniFan
             {
                 if (classPool.Count > 0)
                 {
-                    return classPool.Pop() as T;
+                    var classRef = classPool.Pop();
+#if UNITY_EDITOR
+                    if (!_reusableCheckPool[type].Remove(classRef))
+                    {
+                        UnityEngine.Debug.LogError("ClassPool Get Error: " + type.Name);
+                    }
+#endif
+                    return classRef as T;
                 }
             }
             return new T();
         }
 
-        public static void Put<T>(T data) where T : class, IReusableClass, new()
+        /// <summary>
+        /// 使用泛型的typeof(T)，效率比GetType高，推荐使用
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        public static void Put<T>(T data) where T : class, IReusableClass
         {
             if (data == null) return;
             Type type = typeof(T);
@@ -33,15 +49,41 @@ namespace UniFan
                 UnityEngine.Debug.LogWarning($"ClassPool Put Type Error, Current Type:" + type.Name + " Real Type:" + checkType.Name);
             }
 #endif
+            PutInternal(type, data);
+        }
+
+        /// <summary>
+        /// 使用GetType获取类型，只用于不确定类型的场景
+        /// </summary>
+        /// <param name="data"></param>
+        public static void Put(IReusableClass data)
+        {
+            if (data == null) return;
+            Type type = data.GetType();
+            PutInternal(type, data);
+        }
+
+        private static void PutInternal(Type type, IReusableClass data)
+        {
             Stack<IReusableClass> classPool = null;
             if (!_reusablePool.TryGetValue(type, out classPool))
             {
                 classPool = new Stack<IReusableClass>();
                 _reusablePool[type] = classPool;
+#if UNITY_EDITOR
+                _reusableCheckPool[type] = new HashSet<IReusableClass>();
+#endif
             }
             if (classPool.Count < data.MaxStore)
             {
                 data.OnReset();
+#if UNITY_EDITOR
+                if (!_reusableCheckPool[type].Add(data))
+                {
+                    UnityEngine.Debug.LogError("ClassPool Put Error: " + type.Name);
+                    return;
+                }
+#endif
                 classPool.Push(data);
             }
         }
